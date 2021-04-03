95cc1d51cb7f0a795849dbcfe98608470f8a556c
junwoong <ajx0114@naver.com>
2021-04-03 12:17:45 +0900

    Modify (MyplantsCard.vue)

--- frontend/src/components/PlanIt/MyPlant.vue
+++ frontend/src/components/PlanIt/MyPlant.vue
@@ -1,76 +1,57 @@
 <template>
   <div class="main_component_myplant">
-      <div class="main_component_myplant_title">
-          MY PLANT
-      </div>
-      <div class="main_component_myplant_sub">
-          식물들을 관리해주세요
-      </div>
+    <div class="main_component_myplant_title">MY PLANT</div>
+    <div class="main_component_myplant_sub">식물들을 관리해주세요</div>
 
-      <carousel-3d>
-        <slide class="myplant_slides" :index="0">
-          Slide 1 Content
-        </slide>
-        <slide class="myplant_slides" :index="1">
-          Slide 1 Content
-        </slide>
-        <slide class="myplant_slides" :index="2">
-          Slide 1 Content
-        </slide>
-      </carousel-3d>
-
-      <!-- <carousel-3d>
-          <MyplantsCard
-          v-for="(myplants, idx) in myplant_list"
-          :key="idx"
-          :myplants="myplants"
-        />
-      </carousel-3d> -->
-
-    <MyplantsCard
-      v-for="(myplants, idx) in myplant_list"
-      :key="idx"
-      :myplants="myplants"
-    />  
-
-
+    <flickity
+      v-if="Object.keys(myplant_list).length > 0"
+      ref="flickity"
+      class="carousel"
+      :options="flickityOptions"
+    >
+      <MyplantsCard
+        class="carousel-cell"
+        v-for="(myplants, idx) in myplant_list"
+        :key="idx"
+        :myplants="myplants"
+      />
+    </flickity>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
+import MyplantsCard from "@/components/PlanIt/MyplantsCard.vue";
 
-import MyplantsCard from '@/components/PlanIt/MyplantsCard.vue';
-
-import { Carousel3d, Slide } from 'vue-carousel-3d';
+import Flickity from "vue-flickity";
 
 export default {
   name: "MainMyplant",
 
-  props: ['myplant_list'],
+  props: ["myplant_list"],
 
   data: () => {
     return {
-      where : 1,
-      slides: 5,
-    }
+      where: 1,
+      flickityOptions: {
+        prevNextButtons: false,
+        pageDots: false,
+        wrapAround: true,
+      },
+    };
   },
   components: {
     MyplantsCard,
-    Carousel3d,
-    Slide,
+    Flickity,
   },
-  methods: {
-    
-  },
+  methods: {},
 
   // Mount 이전에, localStorage값이 1인 경우 ( 로그인 안해서 storage에 정보가 들어가지 않은 경우 ) Intro로 돌아가라.
   created() {
-    const token = localStorage.getItem('jwt');
+    const token = localStorage.getItem("jwt");
     if (token == null) {
-      alert('로그인 하고 이용해주세요.');
-      this.$router.push({ name: 'Home' }).catch((error) => {
-        if (error.name === 'NavigationDuplicated') {
+      alert("로그인 하고 이용해주세요.");
+      this.$router.push({ name: "Home" }).catch((error) => {
+        if (error.name === "NavigationDuplicated") {
           location.reload();
         }
       });
