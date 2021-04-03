c174dbe1078df880e1e2d5ed3e97a1a162f21e63
junwoong <ajx0114@naver.com>
2021-04-03 13:55:06 +0900

    Modify (SearchDetail.vue)

--- frontend/src/views/Search/SearchDetail.vue
+++ frontend/src/views/Search/SearchDetail.vue
@@ -18,17 +18,17 @@
 </template>
 
 <script>
-import planticon from '@/assets/img/PLANTIT.png';
-import backicon from '@/assets/img/arrows/left-arrow.png';
-import axios from 'axios';
-import PlantInfo from '@/components/SearchDetail/PlantInfo.vue';
-import PlantCare from '@/components/SearchDetail/PlantCare.vue';
-import PlantProblem from '@/components/SearchDetail/PlantProblem.vue';
+import planticon from "@/assets/img/PLANTIT.png";
+import backicon from "@/assets/img/arrows/left-arrow.png";
+import axios from "axios";
+import PlantInfo from "@/components/SearchDetail/PlantInfo.vue";
+import PlantCare from "@/components/SearchDetail/PlantCare.vue";
+import PlantProblem from "@/components/SearchDetail/PlantProblem.vue";
 
 const SERVER_URL = process.env.VUE_APP_SERVER_URL;
 
 export default {
-  name: 'SearchDetail',
+  name: "SearchDetail",
   data: () => {
     return {
       Plantitimg: planticon,
@@ -50,8 +50,8 @@ export default {
           this.plant_detail = response.data.result;
           // sos 값 가져와서 undefind 제외하고 리스트에 넣기
           for (var i = 1; i <= 8; i++) {
-            var titleName = 'sos_' + i;
-            var detailName = 'sos_solution_' + i;
+            var titleName = "sos_" + i;
+            var detailName = "sos_solution_" + i;
             var temp = new Object();
             if (this.plant_detail[titleName] == undefined) {
               break;
@@ -63,9 +63,9 @@ export default {
           }
         })
         .catch(() => {
-          alert('결과를 가져오는 중에 오류가 발생했습니다');
-          this.$router.push({ name: 'Home' }).catch((error) => {
-            if (error.name === 'NavigationDuplicated') {
+          alert("결과를 가져오는 중에 오류가 발생했습니다");
+          this.$router.push({ name: "Home" }).catch((error) => {
+            if (error.name === "NavigationDuplicated") {
               location.reload();
             }
           });
@@ -75,18 +75,24 @@ export default {
     addMyPlant() {
       axios
         .post(`${SERVER_URL}/search/add/`, {
-          user_number: localStorage.getItem('user_number'),
+          user_number: localStorage.getItem("user_number"),
           plant_id: this.plant_detail.id,
         })
-        .then(({ data }) => {
-          let msg = '등록에 실패하였습니다.';
-          if (data.message === 'success') {
-            msg = '등록에 성공하였습니다.';
+        .then(() => {
+          if (
+            confirm(
+              "식물이 MYPLANT에 등록되었습니다. 해당 페이지로 이동하시겠습니까?"
+            )
+          ) {
+            this.$router.push({ name: "MyPlant" }).catch((error) => {
+              if (error.name === "NavigationDuplicated") {
+                location.reload();
+              }
+            });
           }
-          alert(msg);
         })
         .catch(() => {
-          alert('서버와 통신할 수 없습니다.');
+          alert("서버와 통신할 수 없습니다.");
         });
     },
 
@@ -97,11 +103,11 @@ export default {
 
   // created 할 때 jwt가 없다면 home으로 보내기
   created() {
-    const token = localStorage.getItem('jwt');
+    const token = localStorage.getItem("jwt");
     if (token == null) {
-      alert('로그인 하고 이용해주세요.');
-      this.$router.push({ name: 'Home' }).catch((error) => {
-        if (error.name === 'NavigationDuplicated') {
+      alert("로그인 하고 이용해주세요.");
+      this.$router.push({ name: "Home" }).catch((error) => {
+        if (error.name === "NavigationDuplicated") {
           location.reload();
         }
       });
